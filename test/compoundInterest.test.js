8abf1dd2ec0e70435b60ca11d6dc6263c955138a
Florian PALVADEAU <florian.palvadeau@edu.devinci.fr>
2023-10-18 11:38:52 +0200

    feat(main code, test): adding file and code

---
+++ test/compoundInterest.test.js
@@ -0,0 +1,18 @@
+import { calculateCompoundInterest } from '../src/compoundInterest';
+
+describe('calculateCompoundInterest', () => {
+  test('should correctly calculate compound interest', () => {
+    const startingCapital = 2000;
+    const investingFrequency = 12; // monthly
+    const annualInterestRate = 5; 
+    const investAmount = 100; 
+    const investDuration = 12;
+
+    const result = calculateCompoundInterest(startingCapital, investingFrequency, annualInterestRate, investAmount, investDuration);
+    const finalResult = result.finalAmount
+    const expectedResult = Math.floor(result);
+
+    expect(Number.isFinite(result.finalAmount)).toBe(true);
+    expect(result.finalAmount).toBeCloseTo(expectedResult, 0);
+  });
+});
